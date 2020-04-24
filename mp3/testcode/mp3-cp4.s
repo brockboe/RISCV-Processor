a76e23129c058f384b1bfc2bb2c65e22c2bbc643
Jason Bilas <jasonbilas@gmail.com>
2020-04-24 12:52:41 -0500

    Update MP3 CP4 Testcode

--- mp3/testcode/mp3-cp4.s
+++ mp3/testcode/mp3-cp4.s
@@ -1,4 +1,4 @@
-.align 4
+.align 5
 .section .text
 .globl _start
 
@@ -818,7 +818,7 @@ LoopRowsD:
     HALT:
     beq x0, x0, HALT
 
-
+.align 5
 .section .rodata
 
 XX1:             .word    0x00000000
